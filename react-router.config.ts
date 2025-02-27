57870035e418ba9ad7badf6ec1a249128c14f97f
Gianpierre Fernandez <gianpierrefernandez@gianpierres-macbook-pro.home>
2025-02-27 12:53:33 +0100

    add config for vercel with vercelPreset for deploy

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
+import { vercelPreset } from "@vercel/react-router/vite";
 import { type Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
   prerender: ["/about"],
+  presets: [vercelPreset()]
 } satisfies Config;
